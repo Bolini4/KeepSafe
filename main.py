496ce2a73f3079bfbe58a131f958e7714a823809
Bolini4 <matteo.bolin@gmail.com>
2023-10-02 22:45:29 +0200

    added encrypted files and decrypt

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 import json
+from hashlib import sha256
+import hash_functions
 
 app = Flask(__name__)
+
+def generate_first_json(filename):
+        coffre = filename
+        sites = [
+            {
+                "nom": "Site Web 1",
+                "login": "nom_utilisateur_site1",
+                "mot_de_passe": "mot_de_passe_site1"
+            },
+            {
+                "nom": "Site Web 2",
+                "login": "nom_utilisateur_site2",
+                "mot_de_passe": "mot_de_passe_site2"
+            },
+            {
+                "nom": "Site Web 3",
+                "login": "nom_utilisateur_site3",
+                "mot_de_passe": "mot_de_passe_site3"
+            }
+        ]
+        donnees = {
+        "utilisateur": coffre,
+        "sites": sites
+        }
+        with open("coffres/" + filename + ".json", "w") as f:
+            # Utilisez json.dump pour écrire les données dans le fichier
+            json.dump(donnees, f, indent=2)  # indent=2 pour une indentation lisibl
 
 def list_json_files():
     json_files = []
@@ -15,11 +44,6 @@ def list_json_files():
         pass
     return json_files
 
-coffre_fort = {
-    "utilisateur": "bolini",
-    "mot_de_passe": "coucou"
-}
-
 @app.route("/")
 def index():
     list_json_files()
@@ -30,24 +54,39 @@ def passwordGen():
     return render_template("passge.html")
 
 
-@app.route('/ouvrir', methods=['GET', 'POST'])
-def ouvrir_coffre_fort():
-    if request.method == 'POST':
-        print(request.form['mot_de_passe'])
-        print("coucou")
-        print(coffre_fort['mot_de_passe'])
-        mot_de_passe_saisi = request.form['mot_de_passe']
-        mot_de_passe_coffre = coffre_fort['mot_de_passe']
-        if mot_de_passe_saisi == mot_de_passe_coffre:
-            # Mot de passe correct, redirigez l'utilisateur vers le coffre-fort
-            return redirect(url_for('coffre_fort'))
-        else:
-            # Mot de passe incorrect, affichez un message d'erreur
-            message_erreur = "Mot de passe incorrect. Veuillez réessayer."
-            return render_template('login.html', erreur=message_erreur)
-    return render_template('login.html')
+@app.route('/creer', methods=['POST'])
+def creer_coffre_fort():
+    # Récupérez la valeur soumise pour le champ 'nom' du formulaire
+    nom_du_coffre = request.form.get('nom')
 
-@app.route('/coffre-fort')
-def coffre_fort():
-    # Vous pouvez ajouter ici la logique pour afficher le coffre-fort
-    return "Coffre-fort ouvert !"
+    # Faites quelque chose avec la valeur, par exemple, l'enregistrer dans un fichier JSON
+    # ou dans une base de données
+    return render_template("creer.html", nom_du_coffre=nom_du_coffre)
+
+
+@app.route('/finaliser', methods=['POST'])
+def finaliser_CF():
+    # Récupérez la valeur soumise pour le champ 'nom' du formulaire
+    name = request.form.get('nom')
+    password = request.form.get('password')
+    print(password)
+
+    generate_first_json(name)
+    key = hash_functions.generate_key(password)
+    hash_functions.encrypt("coffres/" + name + ".json", key)
+    return render_template("finaliser.html", name=name, password=password)
+
+@app.route('/ouvrir/<nom_fichier>')
+def ouvrir_coffre_fort(nom_fichier):
+    # La variable "nom_fichier" contiendra le nom du fichier spécifié dans l'URL
+    return render_template("ouvrir.html", nom_fichier=nom_fichier)
+
+@app.route('/show/<nom_fichier>', methods=['POST'])
+def show_coffre_fort(nom_fichier):
+    password = request.form.get('password')
+    key = hash_functions.generate_key(password, load_existing_salt=True)
+    hash_functions.decrypt("coffres/" + nom_fichier + ".json", key) 
+    with open("coffres/"+ nom_fichier + ".json", 'r') as file:
+        data = json.load(file)
+    print(password)
+    return render_template("show.html", data=data)
