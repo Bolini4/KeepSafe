6e634f22f289462d90daafaf0d69e35427c06c7a
Bolini <matteo.bolin@gmail.com>
2023-10-16 17:16:26 +0200

    Cleaned hash_functions.py

--- hash_functions.py
+++ hash_functions.py
@@ -69,39 +69,3 @@ def decrypt(filename, key):
     return 1
     
 
-
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description="File Encryptor Script with a Password")
-    parser.add_argument("file", help="File to encrypt/decrypt")
-    parser.add_argument("-s", "--salt-size", help="If this is set, a new salt with the passed size is generated",
-                        type=int)
-    parser.add_argument("-e", "--encrypt", action="store_true",
-                        help="Whether to encrypt the file, only -e or -d can be specified.")
-    parser.add_argument("-d", "--decrypt", action="store_true",
-                        help="Whether to decrypt the file, only -e or -d can be specified.")
-
-    args = parser.parse_args()
-    file = args.file
-
-    if args.encrypt:
-        password = getpass.getpass("Enter the password for encryption: ")
-    elif args.decrypt:
-        password = getpass.getpass("Enter the password you used for encryption: ")
-
-    if args.salt_size:
-        key = generate_key(password, salt_size=args.salt_size, save_salt=True)
-    else:
-        key = generate_key(password, load_existing_salt=True)
-
-    encrypt_ = args.encrypt
-    decrypt_ = args.decrypt
-
-    if encrypt_ and decrypt_:
-        raise TypeError("Please specify whether you want to encrypt the file or decrypt it.")
-    elif encrypt_:
-        encrypt(file, key)
-    elif decrypt_:
-        decrypt(file, key)
-    else:
-        raise TypeError("Please specify whether you want to encrypt the file or decrypt it.")
